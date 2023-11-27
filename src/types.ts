b3d1d20fb670abf1d10a0396a7cb1e203714635e
Siddharth9890 <siddharth.a9890@gmail.com>
2023-11-27 19:39:55 +0530

    chore:merge main

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,5 @@
 import { FilterPDAInput } from '../.mesh';
 
-import { FilterPDAInput } from '../.mesh';
-
 export enum UserIdentifierType {
   EMAIL = 'EMAIL',
   EVM = 'EVM',
@@ -9,20 +7,6 @@ export enum UserIdentifierType {
   SOLANA = 'SOLANA',
   USER_ID = 'USER_ID',
 }
-
-export enum PDAStatus {
-  'Expired' = 'Expired',
-  'Revoked' = 'Revoked',
-  'Suspended' = 'Suspended',
-  'Valid' = 'Valid',
-}
-
-export type PDAFilter = {
-  filter?: FilterPDAInput;
-  order?: JSON;
-  skip: number;
-  take: number;
-};
 
 export enum PDAStatus {
   'Expired' = 'Expired',
