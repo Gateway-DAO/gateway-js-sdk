35990816fb39c74496c926efaf7254ad7086500f
r11manish <github.jmtss@gmail.com>
2023-11-28 22:17:56 +0530

    done with classes and  95% test  coverage

--- src/Gateway.ts
+++ src/Gateway.ts
@@ -1,10 +1,16 @@
 import { getMeshSDK, Sdk } from '../.mesh';
-import { PDA } from './pda/pda';
-
+import { Auth } from './Auth';
+import { DataModel } from './data-model/data-model';
+import { PDA } from './pda/PDA';
+import { Proof } from './proof/proof';
+import { Request } from './request/request';
 export class Gateway {
-  // public auth: Auth;
-  public pda: PDA;
   private sdk: Sdk;
+  dataModel: DataModel;
+  auth: Auth;
+  pda: PDA;
+  proof: Proof;
+  request: Request;
 
   constructor({ apiKey, token }: { apiKey: string; token: string }) {
     if (!apiKey && !token) throw new Error('No token found');
@@ -12,7 +18,10 @@ export class Gateway {
       apiKey,
       token,
     });
-    // this.auth = new Auth();
+    this.auth = new Auth(this.sdk);
     this.pda = new PDA(this.sdk);
+    this.dataModel = new DataModel(this.sdk);
+    this.proof = new Proof(this.sdk);
+    this.request = new Request(this.sdk);
   }
 }
