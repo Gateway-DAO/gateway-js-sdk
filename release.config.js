70288fe87197c89e785c037fa8eb996ff07f64e7
Manish Rawat <62084296+R11manish@users.noreply.github.com>
2024-02-23 16:14:19 +0530

    chore(commit-analyzer)! : Update release.config.js

--- release.config.js
+++ release.config.js
@@ -7,7 +7,7 @@ module.exports = {
     },
   ],
   plugins: [
-    // '@semantic-release/commit-analyzer',
+    '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     [
       '@semantic-release/changelog',
