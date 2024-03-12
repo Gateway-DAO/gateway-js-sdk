c4fb14b94407407c8e8e917163a05276b05077be
Siddharth9890 <siddharth.a9890@gmail.com>
2024-03-12 14:47:58 +0530

    feat: removed changelog generation

--- release.config.js
+++ release.config.js
@@ -9,12 +9,7 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/changelog',
-      {
-        changelogFile: 'CHANGELOG.md',
-      },
-    ],
+
     '@semantic-release/npm',
     '@semantic-release/github',
     [
