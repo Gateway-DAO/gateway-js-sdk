3795025651e70db5a4f6781319b616940c69bc1a
Manish Rawat <62084296+R11manish@users.noreply.github.com>
2024-03-14 16:51:13 +0530

    chore: added changellog md

--- release.config.js
+++ release.config.js
@@ -9,7 +9,12 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-
+     [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
     '@semantic-release/npm',
     '@semantic-release/github',
     [
