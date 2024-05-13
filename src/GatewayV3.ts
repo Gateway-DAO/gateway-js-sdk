1bb6ddc620080d95ed9fe49d67bb2486b80de412
Siddharth9890 <siddharth.a9890@gmail.com>
2024-05-13 11:43:43 +0530

    feat(GatewayV3): added gateway v3 class

--- src/GatewayV3.ts
+++ src/GatewayV3.ts
@@ -6,10 +6,22 @@ import {
   parameterChecker,
 } from './utils/helper';
 import { PDA } from './v3/pda/pda';
+import { Auth } from './v3/auth/auth';
+import { DataModel } from './v3/data-model/data-model';
+import { Organization } from './v3/organization/organization';
+import { Proof } from './v3/proof/proof';
+import { Request } from './v3/request/request';
+import { User } from './v3/user/user';
 
 export class GatewayV3 {
   private sdk: Sdk;
-  private pda: PDA;
+  public pda: PDA;
+  public auth: Auth;
+  public dataModel: DataModel;
+  public organization: Organization;
+  public proof: Proof;
+  public request: Request;
+  public user: User;
 
   constructor({
     apiKey,
@@ -32,5 +44,11 @@ export class GatewayV3 {
 
     this.sdk = getSdk(client, logging ? clientTimingWrapper : undefined);
     this.pda = new PDA(this.sdk);
+    this.auth = new Auth(this.sdk);
+    this.dataModel = new DataModel(this.sdk);
+    this.organization = new Organization(this.sdk);
+    this.proof = new Proof(this.sdk);
+    this.request = new Request(this.sdk);
+    this.user = new User(this.sdk);
   }
 }
