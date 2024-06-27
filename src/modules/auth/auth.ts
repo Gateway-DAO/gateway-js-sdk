cefeb237814b9a2668b4ce46eecc2296469e1f69
rafagomes <rafaelgomes.web@gmail.com>
2024-06-27 19:16:23 -0300

    refactor: Update import paths for error handling and validator service

--- src/modules/auth/auth.ts
+++ src/modules/auth/auth.ts
@@ -2,21 +2,18 @@ import {
   CreateUserInput,
   Sdk,
   SignedWalletNonceInput,
-} from '../../../gatewaySdk/sources/GatewayV3';
+} from '../../../gatewaySdk/sources/Gateway';
 import { Chain, SignCipherEnum } from '../../common/enums';
-import { errorHandler } from '../../helpers/error-handler';
-import {
-  isDIDValid,
-  isStringValid,
-  isWalletAddressValid,
-  validateObjectProperties,
-} from '../../common/validator-service';
+import { errorHandler } from '../../helpers/helper';
+import { ValidationService } from '../../services/validator-service';
 
 export class Auth {
-  public sdk: Sdk;
+  private sdk: Sdk;
+  private validationService: ValidationService;
 
-  constructor(sdk: Sdk) {
+  constructor(sdk: Sdk, validationService: ValidationService) {
     this.sdk = sdk;
+    this.validationService = validationService;
   }
 
   /**
@@ -25,9 +22,9 @@ export class Auth {
    * needs to be checked for availability.
    * @returns the result of the `checkUsernameAvailability` method, is a boolean
    */
-  async checkUsernameAvailability(username: string) {
+  public async checkUsernameAvailability(username: string) {
     try {
-      isStringValid(username);
+      this.validationService.validateString(username);
       return (await this.sdk.checkUsernameAvailability_query({ username }))
         .checkUsernameAvailability;
     } catch (error) {
@@ -41,9 +38,9 @@ export class Auth {
    * needs to be checked for availability.
    * @returns the result of the `checkDIDAvailability` method, is a boolean
    */
-  async checkDIDAvailability(did: string) {
+  public async checkDIDAvailability(did: string) {
     try {
-      isDIDValid(did);
+      this.validationService.validateDID(did);
       return (await this.sdk.checkDIDAvailability_query({ did }))
         .checkDIDAvailability;
     } catch (error) {
@@ -56,9 +53,9 @@ export class Auth {
    * @param {CreateUserInput} input - The input parameter is of type CreateUserInput
    * @returns the result of the `createUserNonce` method, is a string
    */
-  async createUserNonce(input: CreateUserInput) {
+  public async createUserNonce(input: CreateUserInput) {
     try {
-      validateObjectProperties(input);
+      this.validationService.validateObjectProperties(input);
       return (await this.sdk.createUserNonce_mutation({ input }))
         .createUserNonce;
     } catch (error) {
@@ -73,7 +70,7 @@ export class Auth {
    * @param {SignCipherEnum?} signingCipher - The input parameter is of type SignCipherEnum and is optional
    * @returns the result of the `createUser` method, is a string which has jwt token
    */
-  async createUser({
+  public async createUser({
     signature,
     signingKey,
     signingCipher,
@@ -83,14 +80,9 @@ export class Auth {
     signingCipher?: SignCipherEnum;
   }) {
     try {
-      let chain: Chain;
-      if (signingCipher === undefined) {
-        chain = Chain.EVM;
-      } else if (signingCipher === SignCipherEnum.ED25519) {
-        chain = Chain.SOL;
-      } else chain = Chain.EVM;
-      isWalletAddressValid(signingKey, chain);
-      isStringValid(signature);
+      const chain = this.getChain(signingCipher);
+      this.validationService.validateWalletAddress(signingKey, chain);
+      this.validationService.validateString(signature);
       return (
         await this.sdk.createUser_mutation({
           signature,
@@ -109,15 +101,10 @@ export class Auth {
    * @param {SignCipherEnum?} cipher - The input parameter is of type string which represent wallet
    * @returns the result of the `generateNonce` method, is a string which is a object of type generateNonce_mutationMutation
    */
-  async generateNonce(wallet: string, cipher?: SignCipherEnum) {
+  public async generateNonce(wallet: string, cipher?: SignCipherEnum) {
     try {
-      let chain: Chain;
-      if (cipher === undefined) {
-        chain = Chain.EVM;
-      } else if (cipher === SignCipherEnum.ED25519) {
-        chain = Chain.SOL;
-      } else chain = Chain.EVM;
-      isWalletAddressValid(wallet, chain);
+      const chain = this.getChain(cipher);
+      this.validationService.validateWalletAddress(wallet, chain);
       return await this.sdk.generateNonce_mutation({
         input: { wallet, cipher },
       });
@@ -131,19 +118,21 @@ export class Auth {
    * @param {SignedWalletNonceInput} input - The input parameter is of type SignedWalletNonceInput
    * @returns the result of the `refreshToken` method, is a string which has jwt
    */
-  async refreshToken(input: SignedWalletNonceInput) {
+  public async refreshToken(input: SignedWalletNonceInput) {
     try {
-      let chain: Chain;
-      if (input.cipher === undefined) {
-        chain = Chain.EVM;
-      } else if (input.cipher === SignCipherEnum.ED25519) {
-        chain = Chain.SOL;
-      } else chain = Chain.EVM;
-      isWalletAddressValid(input.signingKey, chain);
-      isStringValid(input.signature);
+      const chain = this.getChain(input.cipher as SignCipherEnum);
+      this.validationService.validateWalletAddress(input.signingKey, chain);
+      this.validationService.validateString(input.signature);
       return (await this.sdk.refreshToken_mutation({ input })).refreshToken;
     } catch (error) {
       throw new Error(errorHandler(error));
     }
+  }
+
+  private getChain(cipher?: SignCipherEnum): Chain {
+    if (cipher === SignCipherEnum.ED25519) {
+      return Chain.SOL;
+    }
+    return Chain.EVM;
   }
 }
