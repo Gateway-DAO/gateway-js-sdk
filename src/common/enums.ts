732f232c80e959c2838d7bceeb0222983c24d3e6
rafagomes <rafaelgomes.web@gmail.com>
2024-06-27 18:35:12 -0300

    feat: Add enums for sign cipher, user identifier type, PDA status, organization identifier type, organization role, auth type, and chain
    This commit adds enums for various types used in the codebase. The enums include SignCipherEnum, UserIdentifierType, PDAStatus, OrganizationIdentifierType, OrganizationRole, AuthType, and Chain. These enums provide a standardized way to represent different types of data in the application.

---
+++ src/common/enums.ts
@@ -0,0 +1,44 @@
+export enum SignCipherEnum {
+  ED25519 = 'ED25519',
+  SECP256K1 = 'SECP256K1',
+}
+
+export enum UserIdentifierType {
+  EMAIL = 'EMAIL',
+  EVM = 'EVM',
+  POK = 'POKT',
+  SOLANA = 'SOLANA',
+  USERNAME = 'USERNAME',
+  USER_DID = 'USER_DID',
+  USER_ID = 'USER_ID',
+}
+export enum PDAStatus {
+  'Expired' = 'Expired',
+  'Revoked' = 'Revoked',
+  'Suspended' = 'Suspended',
+  'Valid' = 'Valid',
+}
+
+export enum OrganizationIdentifierType {
+  ORG_DID = 'ORG_DID',
+  ORG_ID = 'ORG_ID',
+  USERNAME = 'USERNAME',
+}
+
+export enum OrganizationRole {
+  Admin = 'Admin',
+  Member = 'Member',
+  Owner = 'Owner',
+}
+
+export enum AuthType {
+  'EMAIL' = 'EMAIL',
+  'GOOGLE' = 'GOOGLE',
+  'HOT_WALLET' = 'HOT_WALLET',
+  'WALLET' = 'WALLET',
+}
+
+export enum Chain {
+  EVM = 'EVM',
+  SOL = 'SOL',
+}
