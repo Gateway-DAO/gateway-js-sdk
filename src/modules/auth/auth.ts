e78efcb5e4f4c31a7ea96030e08fc9051bed56f5
Siddharth9890 <siddharth.a9890@gmail.com>
2024-09-09 09:45:40 +0530

    chore: added error handler

--- src/modules/auth/auth.ts
+++ src/modules/auth/auth.ts
@@ -2,6 +2,7 @@ import { MediaType } from 'openapi-typescript-helpers';
 import { paths } from '../../api';
 import { Config, ModelAuthRequest, OpenAPIClient } from '../../common/types';
 import { CryptoService } from '../../services/crypto-service';
+import { GTWError } from '../../helpers/custom-error';
 
 export class Auth {
   private client: OpenAPIClient<paths, MediaType>;
@@ -15,19 +16,22 @@ export class Auth {
   }
 
   public async generateSignMessage() {
-    const { data, error } = await this.client.GET('/auth/message');
+    const { data, error, response } = await this.client.GET('/auth/message');
 
     if (error) {
-      throw new Error(error);
+      throw new GTWError(error, response);
     }
 
     return data!.message!;
   }
 
   public async generateRefreshToken() {
-    const { data, error } = await this.client.GET('/auth/refresh-token');
+    const { data, error, response } = await this.client.GET(
+      '/auth/refresh-token',
+    );
+
     if (error) {
-      // throw new Error(JSON.stringify(error));
+      throw new GTWError(error, response);
     }
 
     return data!.token!;
@@ -36,13 +40,12 @@ export class Auth {
   public async login({ message, signature, wallet_address }: ModelAuthRequest) {
     await this.cryptoService.verifyMessage(signature, message, wallet_address);
 
-    const { data, error } = await this.client.POST('/auth', {
+    const { data, error, response } = await this.client.POST('/auth', {
       body: { message, signature, wallet_address },
     });
 
     if (error) {
-      console.log(error);
-      // throw new Error(Object.values(error));
+      throw new GTWError(error, response);
     }
 
     return data!.token!;
--- src/modules/data-asset/data-asset.ts
+++ src/modules/data-asset/data-asset.ts
@@ -6,6 +6,7 @@ import {
 } from '../../common/types';
 import { ValidationService } from '../../services/validator-service';
 import { paths } from '../../api';
+import { GTWError } from '../../helpers/custom-error';
 
 export class DataAsset {
   private client: OpenAPIClient<paths, MediaType>;
@@ -25,9 +26,13 @@ export class DataAsset {
   public async createClaimBasedDataAsset(
     dataAsset: ModelCreateDataAssetRequest,
   ) {
-    const { data } = await this.client.POST('/data-assets', {
+    const { data, error, response } = await this.client.POST('/data-assets', {
       body: dataAsset,
     });
+
+    if (error) {
+      throw new GTWError(error, response);
+    }
 
     return data!;
   }
@@ -35,33 +40,61 @@ export class DataAsset {
   public async createFileBasedDataAsset() {}
 
   public async getDataAssetById(id: number) {
-    const { data } = await this.client.GET('/data-assets/{id}', {
-      params: { path: { id } },
-    });
+    const { data, error, response } = await this.client.GET(
+      '/data-assets/{id}',
+      {
+        params: { path: { id } },
+      },
+    );
+
+    if (error) {
+      throw new GTWError(error, response);
+    }
 
     return data!;
   }
 
   public async updateDataAsset(id: number) {
-    const { data } = await this.client.PUT('/data-assets/{id}', {
-      params: { path: { id: id.toString() } },
-    });
+    const { data, error, response } = await this.client.PUT(
+      '/data-assets/{id}',
+      {
+        params: { path: { id: id.toString() } },
+      },
+    );
+
+    if (error) {
+      throw new GTWError(error, response);
+    }
 
     return data!;
   }
 
   public async deleteDataAsset(id: number) {
-    const { data } = await this.client.DELETE('/data-assets/{id}', {
-      params: { path: { id: id.toString() } },
-    });
+    const { data, error, response } = await this.client.DELETE(
+      '/data-assets/{id}',
+      {
+        params: { path: { id: id.toString() } },
+      },
+    );
+
+    if (error) {
+      throw new GTWError(error, response);
+    }
 
     return data!;
   }
 
   public async downloadDataAsset(id: number) {
-    const { data } = await this.client.GET('/data-assets/{id}/download', {
-      params: { path: { id } },
-    });
+    const { data, error, response } = await this.client.GET(
+      '/data-assets/{id}/download',
+      {
+        params: { path: { id } },
+      },
+    );
+
+    if (error) {
+      throw new GTWError(error, response);
+    }
 
     return data!;
   }
